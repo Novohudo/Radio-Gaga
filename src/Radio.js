ae3bb0ff1a0e12a31c484d9ded63c8f938a31cf3
Oleksii Umrikhin <89519243+Novohudo@users.noreply.github.com>
2023-01-22 13:00:47 +0100

    Radio.js component

--- src/Radio.js
+++ src/Radio.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {RadioBrowserApi} from "radio-browser-api";
+import H5AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css"
+import error from "./img/error.png"
 
-const Radio = () => {
+export default function Radio() {
+	const [stations, setStations] = useState(null);
+	const [stationFilter, setStationFilter] = useState("all")
+	const [stream,setStream] = useState('')
+	const filters = [
+		"all",
+		"classical",
+		"country",
+		"dance",
+		"disco",
+		"house",
+		"jazz",
+		"pop",
+		"rap",
+		"retro",
+		"rock",
+	]
+
+	useEffect(() => {
+		setupApi(stationFilter).then(data => {
+			setStations(data)
+		})
+	}, [stationFilter])
+
+	const setupApi = async stationFilter => {
+		const api = new RadioBrowserApi("My Radio App")
+		const stations = await api.searchStations({
+			language: "english",
+			tag: stationFilter,
+			limit: 30,
+		})
+		.then(data => {
+			return data
+		})
+		return stations
+	}
+	const setDefaultSrc = event => {
+		event.target.src = error
+	}
+
 	return (
-		<div>
-			A!
-		</div>
-	);
-};
+		<div className="radio">
+			<H5AudioPlayer
+				header={stream.name}
+				className={"player"}
+				src={stream.urlResolved}
+				showJumpControls={false}
+				layout={"stacked"}
+				customProgressBarSection={[]}
+				customControlsSection={["MAIN_CONTROLS","VOLUME_CONTROLS"]}
+				autoPlayAfterSrcChange={true}
+			/>
+			<div className="filters">
+				{filters.map((filter, index) => (
+					<span
+						key={index}
+						className={stationFilter === filter ? "selected" : ""}
+						onClick={() => setStationFilter(filter)}
+					>
+          {filter}
+        </span>
+				))}
+			</div>
+			<div className="stations">
+				{stations &&
+					stations.map((station, index) => {
 
-export default Radio;
+						return (
+							<div className="station" key={index} onClick={()=>setStream(station)}>
+								<div className="stationName">
+									<img
+										className="logo"
+										src={station.favicon}
+										alt="station logo"
+										onError={setDefaultSrc}
+									/>
+									<div className="name">{station.name}</div>
+								</div>
+							</div>
+						)
+					})}
+
+			</div>
+
+		</div>
+	)
+};
